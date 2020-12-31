cca41977afe812c9fe5140f41bc7524c05c71cb6
Siddharth Gampa <siddharth_gampa@yahoo.com>
2020-12-30 21:48:12 -0800

    changed some css, added 1 more table

--- Tooth Chart/ToothChart.js
+++ Tooth Chart/ToothChart.js
@@ -28,7 +28,7 @@ class Grid extends React.Component {
 
   render() {
     return (
-    <div> Tooth Chart
+    <div>
         <form action="">
         <div className="grid">
           <GridRow name="1" onChange={this.onChange}/>
@@ -49,6 +49,28 @@ class Grid extends React.Component {
           <GridRow name="16" onChange={this.onChange}/>
           <Labels/>
           </div>
+
+          <div className="grid">
+            <GridRow name="32" onChange={this.onChange}/>
+            <GridRow name="31" onChange={this.onChange}/>
+            <GridRow name="30" onChange={this.onChange}/>
+            <GridRow name="29" onChange={this.onChange}/>
+            <GridRow name="28" onChange={this.onChange}/>
+            <GridRow name="27" onChange={this.onChange}/>
+            <GridRow name="26" onChange={this.onChange}/>
+            <GridRow name="25" onChange={this.onChange}/>
+            <GridRow name="24" onChange={this.onChange}/>
+            <GridRow name="23" onChange={this.onChange}/>
+            <GridRow name="22" onChange={this.onChange}/>
+            <GridRow name="21" onChange={this.onChange}/>
+            <GridRow name="20" onChange={this.onChange}/>
+            <GridRow name="19" onChange={this.onChange}/>
+            <GridRow name="18" onChange={this.onChange}/>
+            <GridRow name="17" onChange={this.onChange}/>
+            <Labels/>
+
+
+            </div>
           <input id="submit" type="submit" onClick={this.onSubmit}/>
           </form>
           <p>{JSON.stringify(this.state.grid)}</p>
@@ -65,23 +87,22 @@ class GridRow extends React.Component {
   render() {
     return (
       <div>
-      <input type="text" id={this.props.name+"~"+1} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+2} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+3} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+4} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+5} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+6} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+7} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+8} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+9} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+10} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+11} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+12} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+13} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+14} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+15} maxLength="3" onChange={this.props.onChange}/>
-      <input type="text" id={this.props.name+"~"+16} maxLength="3" onChange={this.props.onChange}/>
-      <div>{this.props.name}</div>
+      <input type="number" max="999" id={this.props.name+"~"+1} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+2} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+3} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+4} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+5} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+6} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+7} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+8} maxLength="3" onChange={this.props.onChange}/>
+      <div className="name">{this.props.name}</div>
+      <input type="number" max="999" id={this.props.name+"~"+9} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+10} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+11} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+12} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+13} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+14} maxLength="3" onChange={this.props.onChange}/>
+      <input type="number" max="999" id={this.props.name+"~"+15} maxLength="3" onChange={this.props.onChange}/>
       </div>
     )
   }
@@ -92,14 +113,16 @@ class Labels extends React.Component {
     super(props)
   }
   render() {
-    return (<div>
-    <div>Progn</div>
-    <div>Diagnosis</div>
+    return (<div className="labels">
+    <div >Progn</div>
+    <div >Diagnosis</div>
     <div>Furcation</div>
     <div>Bleed</div>
     <div>GM/Rec</div>
     <div>Cal</div>
     <div>PD</div>
+    <div>Plaque</div>
+    <div >------------</div>
     <div>Plaque</div>
     <div>PD</div>
     <div>Cal</div>
@@ -107,9 +130,6 @@ class Labels extends React.Component {
     <div>Bleed</div>
     <div>Furcation</div>
     <div>Mobil</div>
-    <div>Progn</div>
-    <div>Progn</div>
-    <div>Progn</div>
 
     </div>
   )
