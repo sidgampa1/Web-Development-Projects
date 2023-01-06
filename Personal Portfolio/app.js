6e41c1b1c8e78b6b157a8d554a6b54f22cbc6245
Sid Gampa <sgampa@twilio.com>
2023-01-06 14:51:35 -0800

    Finished Header section 101

--- Personal Portfolio/app.js
+++ Personal Portfolio/app.js
@@ -1,19 +1,35 @@
-console.log("js script beginning");
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
-console.log("loaded js script");
+
 function pageTransitions() {
-  console.log("adding event listeners");
   for(let i = 0; i < sectBtn.length; i++) {
     sectBtn[i].addEventListener('click', function() {
-      console.log('handling active-btn');
       let currentBtn = document.querySelectorAll('.active-btn');
       currentBtn[0].classList.remove('active-btn');
       this.classList.add('active-btn');
     })
   }
 }
+
+allSections[0].addEventListener('click', (e) => {
+  const id = e.target.dataset.id;
+  if (id){
+    //remove selected from other btns
+    sectBtns.forEach((btn) => {
+      btn.classList.remove('active')
+    })
+    e.target.classList.add('active')
+
+    //hide other sections
+    sections.forEach((section) => {
+      section.classList.remove('active')
+    })
+
+    const element = document.getElementById(id);
+    element.classList.add('active');
+  }
+})
 
 pageTransitions();
